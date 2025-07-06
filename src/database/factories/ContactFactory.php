665fadb6b4b3918e6ed3be625ab545f9bf820a49
mayu <27mys2@gmail.com>
2025-07-06 16:30:30 +0900

    ContactFactory更新

--- src/database/factories/ContactFactory.php
+++ src/database/factories/ContactFactory.php
@@ -8,27 +8,20 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class ContactFactory extends Factory
 {
     protected $model = Contact::class;
-    protected $faker;
-
-    public function __construct(...$args)
-    {
-        parent::__construct(...$args);
-        $this->faker = \Faker\Factory::create('ja_JP');
-    }
 
     public function definition()
     {
 
         return [
-            'category_id' => $faker->numberBetween(1, 5),
-            'first_name' => $faker->lastName(),
-            'last_name' => $faker->firstName(),
-            'gender' => $faker->randomElement([1, 2, 3]),
-            'email' => $faker->safeEmail(),
-            'tell' => $faker->phoneNumber(),
-            'address' => $faker->prefecture . $faker->city . $faker->streetAddress,
-            'building' => $faker->secondaryAddress(),
-            'detail' => $faker->sentence(3),
+            'category_id' => $this->faker->numberBetween(1, 5),
+            'first_name' => $this->faker->lastName(),
+            'last_name' => $this->faker->firstName(),
+            'gender' => $this->faker->randomElement([1, 2, 3]),
+            'email' => $this->faker->safeEmail(),
+            'tell' => $this->faker->phoneNumber(),
+            'address' => $this->faker->prefecture . $this->faker->city . $this->faker->streetAddress,
+            'building' => $this->faker->secondaryAddress(),
+            'detail' => $this->faker->realText(120),
         ];
     }
 }
